663352249f25174342ed305b07d9b74cec5e1c9b
colby <colby@10.1.10.3>
2016-10-15 17:14:40 -0500

    Made copy-paste modifications as requested

--- Lab6_final/src/lab6_final/Inventory.java
+++ Lab6_final/src/lab6_final/Inventory.java
@@ -18,31 +18,29 @@ public class Inventory
         //averageCost();
         while(answer == 'Y' || answer == 'y') 
             {
-                 System.out.println("number of item types in the inventory ->");
-                 calculator.setNumber(sc.nextInt());
-                 //for(int i = 1; i <= number; i++)
-                 //{
-                System.out.println("enter the item's description");
-                item = sc.next();
-                System.out.println("item description: " + item);
-                System.out.println("enter item quantity");
-                calculator.setQuantity(sc.nextInt());
-                //sumItems += quantity;
-                System.out.println("enter item cost");
-                calculator.setCost(sc.nextDouble());
-                //totValue += cost * quantity;
-               /// }
-            // begin code block to display results
-            System.out.printf("average cost: $%.2f\n", calculator.computeAverage());
-            System.out.print("At a level of " + calculator.getNumber()*calculator.getQuantity() + " units, ");
-            System.out.printf("the inventory tax is : $%.2f\n", calculator.computeInvTax());
-            System.out.println("***************************");
-            // end code block to display results
-            // begin code block to perform additional program run 
-            System.out.println("run again(Y or N)?");
-            answer = sc.next().charAt(0);
-            // end code block to perform additional program run 
-            }
+            System.out.println("number of item types in the inventory ->");
+            calculator.setNumber(sc.nextInt());
+            //for(int i = 1; i <= number; i++)
+            //{
+            System.out.println("enter the item's description");
+            item = sc.next();
+            System.out.println("item description: " + item);
+            System.out.println("enter item quantity");
+            calculator.setQuantity(sc.nextInt());
+            System.out.println("enter item cost");
+            calculator.setCost(sc.nextDouble());
+            //}
+        // begin code block to display results
+        System.out.printf("average cost: $%.2f\n", calculator.computeAverage());
+        System.out.print("At a level of " + calculator.getNumber()*calculator.getQuantity() + " units, ");
+        System.out.printf("the inventory tax is : $%.2f\n", calculator.computeInvTax());
         System.out.println("***************************");
-    }// end main() method
+        // end code block to display results
+        // begin code block to perform additional program run 
+        System.out.println("run again(Y or N)?");
+        answer = sc.next().charAt(0);
+        // end code block to perform additional program run 
+        }  
+    }
+}// end main() method
 
