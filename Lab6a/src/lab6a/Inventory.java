9c29d5fe13bdc25a32b477efcd238913ba1c619e
colby <colby@10.1.10.3>
2016-10-15 11:58:48 -0500

    Made copy-paste modifications as requested

--- Lab6a/src/lab6a/Inventory.java
+++ Lab6a/src/lab6a/Inventory.java
@@ -8,42 +8,65 @@ import java.util.Scanner;
 //Colby Underhill, Programmer
 public class Inventory 
     {
+    //static int numItems = 0;
+    //static double avgInvCost = 0;
     static Scanner sc = new Scanner(System.in);
     static Calc calculator = new Calc();
+    static DisplayMenu menu = new DisplayMenu();
     public static void main(String args[])
-        {
-            char answer = 'Y';
-            //DisplayMenu();
-            while(answer == 'Y' || answer == 'y') 
+    {
+        // begin local variable declaration / initialization zone
+        char answer = 'Y';
+        // end local variable declaration / initialization zone
+        menu.getMenu();
+        while(answer == 'Y' || answer == 'y') 
             {
-            int number = 0;
-            String item = "";
-            // begin code block for inventory evaluation
-            System.out.println("number of item types in the inventory ->");
-            calculator.setNumber(sc.nextInt());
-            System.out.println("items " + calculator.getNumber());
-
-            System.out.println("enter item quantity");
-            calculator.setQuantity(sc.nextInt());
-            System.out.println("quantity " + calculator.getQuantity());
-
-            System.out.println("sumitems " + calculator.getSumItems());
-            
-            System.out.println("enter item cost");
-            calculator.setCost(sc.nextDouble());
-            System.out.println("cost " + calculator.getCost());
-            
-            System.out.println("totvalue " + calculator.getTotValue());
-            }
             // begin code block to display results
             System.out.printf("average cost: $%.2f\n", calculator.computeAverage());
-            
             System.out.print("At a level of " + calculator.getTotItems() + " units, ");
-            
             System.out.printf("the inventory tax is : $%.2f\n", calculator.computeInvTax());
             System.out.println("***************************");
             // end code block to display results
             // begin code block to perform additional program run 
+            System.out.println("run again(Y or N)?");
+            answer = sc.next().charAt(0);
+            // end code block to perform additional program run 
+            }
+        System.out.println("***************************");
+    }// end main() method
 
-        }
-}
+    static double averageCost()
+        {
+        //double average = 0.0, cost = 0.0, totValue = 0.0;
+        int number = 0;// sumItems = 0, quantity = 0;
+        String item = "";
+        // begin code block for inventory evaluation
+        System.out.println("number of item types in the inventory ->");
+        calculator.setNumber(sc.nextInt());
+        for(int i = 1; i <= number; i++)
+            {
+            System.out.println("enter the item's description");
+            item = sc.next();
+            System.out.println("item description: " + item);
+            System.out.println("enter item quantity");
+            calculator.setQuantity(sc.nextInt());
+            //sumItems += quantity;
+            calculator.setSumItems(3);
+            System.out.println("enter item cost");
+            calculator.setCost(sc.nextDouble());
+            //totValue += cost * quantity;
+            }
+        //numItems = sumItems;
+        //avgInvCost = totValue / sumItems;
+        // outside the for() loop
+        //average = totValue / sumItems;
+        // end code block for inventory evaluation
+        return 0;
+    }//end method
+   // static double computeTax() 
+    //    {
+    //   double tax = 0;
+    //    tax = avgInvCost * .06;
+    //    return tax;
+     //   }//end method
+}// end class
